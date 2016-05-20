e54efc915ce3afe4db173ee32aaa3683b30dbf53
Stephen Cooney <lokistriker@hotmail.co.uk>
2016-05-20 10:54:57 +0100

    StartedInterface

---
+++ unit19_gameProject/interface.cpp
@@ -0,0 +1,22 @@
+
+#include "interface.h"
+#include "characterEntity.h"
+
+interface::interface(){
+
+}
+
+interface::interface(){
+
+}
+
+interface::~interface(){
+
+}
+int interface::displayHealth() {
+	
+}
+int interface::displayStats() {
+
+}
+#pragma once
---
+++ unit19_gameProject/interface.h
@@ -0,0 +1,31 @@
+#ifndef INTERFACE_HEADER
+#define INTERFACE_HEADER
+
+#include <iostream>
+#include <string>
+#include <SDL.h>
+using namespace std;
+/*******************************************
+
+
+*******************************************/
+
+class interface
+{
+public:
+
+	interface();
+	virtual ~interface();
+	int displayHealth();
+	int displayStats();
+protected:
+
+
+
+private:
+
+};
+#endif //End of menu Header
+
+
+
